202add4cc2d3637fe5699c267fb47a150d333b9c
Alexander Apostolov <alexanderapostolov@protonmail.com>
2017-10-22 10:21:56 -0400

    Init commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+// Use Node.js built-in module "path" to build cleaner paths between "server" & "public" directories
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
